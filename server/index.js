5d4330d759da880b5f4b03705dc20cc6de6abc64
Brian Macdonald <brian.lmacdonald@gmail.com>
2017-12-29 11:38:12 -0500

    adds start script for server

--- server/index.js
+++ server/index.js
@@ -1,1 +1,11 @@
 const https = require('https')
+const PORT = process.env.PORT || 1337
+
+const options = {}
+
+https.createServer(options, (req, res) => {
+  res.writeHead(200);
+  res.end('done.')
+  console.log(`listening on ${PORT}!`)
+}).listen(PORT)
+
