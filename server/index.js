44cfa79dfb7cdfc56a982873d8416df3e269600d
Brian Macdonald <brian.lmacdonald@gmail.com>
2018-01-04 19:32:46 -0500

    sets up linting rules

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const morgan = require('morgan')
 const routes = require('./routes')
 const bodyParser = require('body-parser')
+const path = require('path')
 
 const PORT = process.env.PORT || 1337
 const app = express()
@@ -12,6 +13,7 @@ app.use(bodyParser.urlencoded({
   extended: true
 }));
 app.use('/api', routes)
+app.use(express.static(path.join(__dirname, 'public')))
 
 app.use((error, req, res, next) => {
   res.json({error: error.message})
