8eb8eca0867d733528c998c45b4c1e4123b00b41
Brian Macdonald <brian.lmacdonald@gmail.com>
2018-01-04 19:58:55 -0500

    configures linter

--- server/index.js
+++ server/index.js
@@ -8,10 +8,10 @@ const PORT = process.env.PORT || 1337
 const app = express()
 
 app.use(morgan('dev'))
-app.use(bodyParser.json());
+app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({
   extended: true
-}));
+}))
 app.use('/api', routes)
 app.use(express.static(path.join(__dirname, 'public')))
 
@@ -19,6 +19,4 @@ app.use((error, req, res, next) => {
   res.json({error: error.message})
 })
 
-app.listen(PORT, () => {
-  console.log(`listening on ${PORT} and running in:`, process.env.NODE_ENV)
-})
+app.listen(PORT)
