fc504f2765085a5cd40e07d9b30a901befeb4ffa
Brian Macdonald <brian.lmacdonald@gmail.com>
2018-05-01 10:22:44 -0400

    adds flow to project reducer

---
+++ Types/Project.js
@@ -0,0 +1,7 @@
+// @flow
+'use strict';
+export type ProjectNode = Object;
+export type ProjectArray = Array<ProjectNode>;
+export type ProjectError = Object;
+export type ProjectPath = string;
+export type ProjectPathArray = Array<ProjectPath>;
