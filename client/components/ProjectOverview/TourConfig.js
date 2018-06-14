950c49b00db2ec5bea5a5490c242eab72cd5f94b
Brian Macdonald <brian.lmacdonald@gmail.com>
2018-06-14 15:32:05 -0400

    creates config for card view pathname

--- client/components/ProjectOverview/TourConfig.js
+++ client/components/ProjectOverview/TourConfig.js
@@ -1,13 +1,37 @@
 'use strict';
 
 const config = {
-  0: {
-    id: 'reorder',
-    description: 'reorders index cards'
-  },
-  1: {
-    id: 'fullview',
-    description: 'shows the full project'
+  '/projects': {
+    0: {
+      id: 'cardview',
+      parent: 'nav-ul',
+      description: 'views index cards'
+    },
+    1: {
+      id: 'fullview',
+      parent: 'nav-ul',
+      description: 'shows full project'
+      },
+    2: {
+      id: 'reorder',
+      parent: 'nav-ul',
+      description: 'reorders index cards'
+    },
+    3: {
+      id: 'help',
+      parent: 'nav-ul',
+      description: 'displays help bubbles'
+    },
+    4: {
+      id: 'add-PROJECT_TYPE',
+      parent: 'PROJECT_TYPE-container',
+      description: 'click to create new project'
+    },
+    5: {
+      id: 'thumbnail-PROJECT_TYPE-0',
+      parent: 'PROJECT_TYPE-container',
+      description: 'double click card to open child index cards'
+    }
   }
 };
 
