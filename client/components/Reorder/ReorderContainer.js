c903fa76823c5ae6fabf735f2347ddf01e4c6a3f
Brian Macdonald <brian.lmacdonald@gmail.com>
2018-06-21 10:38:56 -0400

    passes only required props to reorder card

--- client/components/Reorder/ReorderContainer.js
+++ client/components/Reorder/ReorderContainer.js
@@ -4,16 +4,14 @@ import { connect } from 'react-redux';
 import PropTypes from 'prop-types'
 import { DropTarget, DragDropContext } from 'react-dnd'
 import HTML5Backend from 'react-dnd-html5-backend'
-import Card from 'APP/client/components/Reorder/ReorderCard'
+import { CardDragDrop as Card } from 'APP/client/components/Reorder/ReorderCard'
 import LoaderHOC from 'APP/client/components/HOC/LoaderHOC';
 import update from 'immutability-helper';
 import { List } from 'immutable';
-import { updateOrder } from 'APP/client/store/reducers/order';
 import {
   CLASS_NAME_OBJ
 } from 'APP/client/components/HierarchyControl/CardTypes';
 import acceptedDrop from 'APP/client/components/Reorder/CardConstants';
-import { changeParent } from 'APP/client/store/actions/changeParent';
 
 const getItemType = (props) => {
 	return props.type;
@@ -38,21 +36,23 @@ class Container extends Component {
 		super(props)
 		this.moveCard = this.moveCard.bind(this)
 		this.findCard = this.findCard.bind(this)
-		const { handleOrder, type, thumbs } = props;
+		const { handleOrder, type, thumbs, navigator } = props;
 		const startingOrder = { type, list: thumbs }
 		handleOrder(startingOrder);
 		this.state = {
-			thumbs: thumbs
+			thumbs: thumbs,
+			navigator: navigator
 		}
 	}
 
 	static getDerivedStateFromProps(nextProps, state){
-    if (nextProps.thumbs.size !== state.thumbs.size) {
+    if (nextProps.thumbs.size !== state.thumbs.size || nextProps.navigator !== state.navigator) {
       nextProps.handleOrder({type: nextProps.type, list: nextProps.thumbs});
       return {
-        thumbs: nextProps.thumbs
+				thumbs: nextProps.thumbs,
+				navigator: nextProps.navigator
       };
-    }
+		}
     return null;
   }
 
@@ -76,38 +76,55 @@ class Container extends Component {
 	}
 
 	render() {
-		const { connectDropTarget, type, order, index } = this.props;
+		console.log('rendered');
+		const { 
+			connectDropTarget,
+			handleChangeParent,
+			handleOrder,
+			type,
+			order,
+			index,
+			user,
+			navigator,
+			handleNavigation,
+			handleHotSeat,
+			parent 
+		} = this.props;
 		const cards = order.get(type) || List([]);
+		console.log('parent', parent);
 		return connectDropTarget(
 				<div id={`${type}-reorder-container`} className='subContainer'>
-				{cards.map((card, idx) => (
-					<div key={idx} id={`reorder-${type}-${idx}`}><Card
-						accepts={acceptedDrop[type]}
-						canDrag={type !== 'PROJECT_TYPE'}
-						{...this.props}
-						card={card}
-						key={card.get('id')}
-						id={card.get('id')}
-						text={card.get('body')}
-						moveCard={this.moveCard}
-						findCard={this.findCard}
-					/>
+				{cards.map((card, idx) => {
+					console.log(card);
+					return (
+					<div key={idx} id={`reorder-${type}-${idx}`}>
+						<Card
+							type={card.get('type')}
+							accepts={acceptedDrop[type]}
+							canDrag={type !== 'PROJECT_TYPE'}
+							userId={user.get('id')}
+							parent={parent}
+							body={card.get('body')}
+							title={card.get('title')}
+							key={card.get('id')}
+							id={card.get('id')}
+							handleChangeParent={handleChangeParent}
+							handleOrder={handleOrder}
+							handleHotSeat={handleHotSeat}
+							handleNavigation={handleNavigation}
+							currentNavId={navigator.get(type)}
+							projectId={navigator.get('PROJECT_TYPE')}
+							moveCard={this.moveCard}
+							findCard={this.findCard}
+						/>
 					</div>
-				))}
+				)})}
 				</div>
 		)
 	}
 }
 
-const mapDispatch = dispatch => ({
-	handleOrder(updateObj){
-		dispatch(updateOrder(updateObj))
-	},
-	handleChangeParent(updateObj){
-		dispatch(changeParent(updateObj))
-	}
-});
-
 const ContainerTarget = DropTarget((props) => {return props.type}, cardTarget, collect)(Container);
 const ContainerContext = DragDropContext(HTML5Backend)(ContainerTarget);
-export default connect(null, mapDispatch)(LoaderHOC('thumbs')(ContainerContext));
+const LoadingReorderContainer = LoaderHOC('thumbs')(ContainerContext);
+export default LoadingReorderContainer;
