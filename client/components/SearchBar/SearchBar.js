d7536eb0f98769a358650394da0c1d47715709da
Brian Macdonald <brian.lmacdonald@gmail.com>
2018-07-29 20:57:03 -0400

    modifies search from client to server

--- client/components/SearchBar/SearchBar.js
+++ client/components/SearchBar/SearchBar.js
@@ -1,45 +1,76 @@
 'use strict';
 import React, { Component } from 'react';
 import Suggestions from 'APP/client/components/SearchBar/Suggestions';
-import { debounce } from 'lodash';
-import findRelevant from 'APP/client/components/SearchBar/findRelevant';
+import axios from 'axios';
+import 'APP/client/components/SearchBar/SearchBar.css';
+import { projectLoaded } from 'APP/client/store/reducers/project';
+import { connect } from 'react-redux';
 
 class Search extends Component {
   constructor(props){
     super(props);
     this.state = {
       query: '',
-      results: []
+      results: [],
+      searchAll: true,
+      hide: false
     }
     this.handleInputChange = this.handleInputChange.bind(this);
+    this.resetState = this.resetState.bind(this);
+    this.handleSearchDepth = this.handleSearchDepth.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  resetState(){
+    this.setState({query: '', results: []});
+  }
+
+  handleSubmit(){
+    const { user, navigator, handleSearchAll } = this.props;
+    this.state.searchAll ?
+      this.setState({results: handleSearchAll(user.get('id'), this.state.query)}) :
+      this.setState({results: searchActive(user.get('id'), navigator.get('PROJECT_TYPE'), this.state.query)});
+
   }
 
   handleInputChange(){
-    const { navigator, project } = this.props;
-    const currentProject = project.get('userProjects').find((proj) => proj.get('id') === navigator.get('PROJECT_TYPE'));
+    this.setState({ query: this.search.value });
+  }
 
-    if (currentProject === undefined) return;
-    
-    this.setState({
-      query: this.search.value
-    }, debounce(() => {
-      if (this.state.query && this.state.query.length > 1) {
-          this.setState({results: findRelevant(currentProject, this.state.query)});
-      }
-    }), 500);
+  handleSearchDepth(){
+    this.setState({searchAll: !this.state.searchAll})
   }
 
   render(){
-    const { navigator } = this.props;
-    const disable = navigator === undefined || navigator.get('PROJECT_TYPE' === null);
 
     return (
       <form>
-        <input
-          placeholder='search for...'
-          ref={input => this.search = input}
-          onChange={this.handleInputChange}
-        />
+        <div className='field has-addons'>
+          <div className='control'>
+            <input
+              id='search-input'
+              className='input'
+              placeholder='search for...'
+              ref={input => this.search = input}
+              onChange={this.handleInputChange}
+              onBlur={this.resetState}
+            />
+          </div>
+          <div className='control'>
+            <a
+              className={`button ${this.state.searchAll ? 'is-info' : 'is-info is-inverted is-outlined'}`}
+              onClick={this.handleSearchDepth}
+              >
+              {'search all'}
+            </a>
+            <a
+              className='button is-success'
+              onClick={this.handleSubmit}
+              >
+              go
+            </a>
+          </div>
+        </div>
         <Suggestions results={this.state.results}/>
       </form>
     );
@@ -47,4 +78,33 @@ class Search extends Component {
 
 }
 
-export default Search;
+function searchActive(userId, projectId, term) {
+  return axios.get(`/api/search/single-project/${userId}/${projectId}/${term}`)
+    .then(results => {
+      return results.data.hits;
+    })
+    .catch(console.error)
+}
+
+function searchAll(userId, term) {
+  return function (dispatch) {
+    return axios.get(`/api/search/all-projects/${userId}/${term}`)
+      .then(allResults => {
+        if (allResults.data.projects.length) {
+          allResults.data.projects.forEach(project => {
+            dispatch(projectLoaded(project))
+          })
+        }
+        return allResults.data.hits;
+      })
+      .catch(console.error)
+  }
+}
+
+const mapDispatch = dispatch => ({
+  handleSearchAll: function(userId, term){
+    return dispatch(searchAll(userId, term));
+  }
+});
+
+export default connect(null, mapDispatch)(Search);
--- client/components/SearchBar/Suggestions.js
+++ client/components/SearchBar/Suggestions.js
@@ -1,15 +1,33 @@
 'use strict';
 import React from 'react';
+import 'APP/client/components/SearchBar/Suggestions.css';
 
 const Suggestions = (props) => {
 
-  const matches = props.results.map(r => (
-    <li key={r.hit.get('id')}>
-      {r.hit.get('title')}
-    </li>
-  ));
+  const matches = props.results.map(projHit => {
+    if (projHit.found.length === 0) return;
+    return(<li key={projHit.project}>
+            <h4 className='has-text-success'>{projHit.project}</h4>
+            <ul>{projHit.found.map(r => (
+              <li key={r.hit.get('id')}>
+                <div>
+                  <div>
+                    <h4>{r.hit.get('title')}</h4>
+                    <p>{r.hit.get('body')}</p>
+                  </div>
+                </div>
+              </li>))}
+            </ul>
+          </li>
+  )});
+  if (matches.length === 0) return <div />;
 
-  return <ul>{matches}</ul>;
+  return (
+    <div className='suggestions-div'>
+      <div className='suggestion-bubble'>
+        <ul className='suggestion-list'>{matches}</ul>
+      </div>
+    </div>);
 };
 
 export default Suggestions;
